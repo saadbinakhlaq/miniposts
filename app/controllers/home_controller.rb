dafbb53b62d774db0b68aa6d7702998993dd6e25
Saad Bin Akhlaq <saad.18@gmail.com>
2016-06-22 13:07:36 +0530

    feature(devise_views): Add custom devise views styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,8 @@ class HomeController < ApplicationController
   def checked_logged_in_user
     if current_user
       redirect_to dashboard_path
+    else
+      redirect_to user_session_path
     end
   end
 end
