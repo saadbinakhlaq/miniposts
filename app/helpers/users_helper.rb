a5437f80dc7cc925021f46af40b36710c9e70d48
Saad Bin Akhlaq <saad.18@gmail.com>
2016-06-22 13:57:12 +0530

    fix(styles): Correct styling of Pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def follow_button(user)
     if current_user.following? user
-      button_to t('.unfollow'), user_follow_path(user), method: :delete
+      button_to t('.unfollow'), user_follow_path(user), method: :delete, class: 'pure-button follow_button'
     else
-      button_to t('.follow'), user_follow_path(user)
+      button_to t('.follow'), user_follow_path(user), class: 'pure-button follow_button'
     end
   end
 end
